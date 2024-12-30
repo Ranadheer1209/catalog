16b956006660bcfb6271ee2ad3353660f345714b
Ranadheer1209 <137439890+Ranadheer1209@users.noreply.github.com>
2024-12-30 12:21:56 +0530

    Update import.js

--- import.js
+++ import.js
@@ -1,13 +1,11 @@
 
 const fs = require('fs');
 
-// Method to interpret a value depending on the specified base
 
 function basedecoder(value, base) {
     return parseInt(value, base);
 }
 
-// Method to perform Lagrange interpolation and determine the constant phase (c)
 
 function lagrangeInterpolation(numbers) {
     let fixed = 0;
@@ -29,7 +27,7 @@ function lagrangeInterpolation(numbers) {
     return Math.round(fixed);
 }
 
-// Method to interpret the input and compute the constant phase
+
 
 function solvedequation(filename) {
     const content = fs.readFileSync(filename);
@@ -38,7 +36,7 @@ function solvedequation(filename) {
     const { n, k } = input.keys;
     let numbers = [];
 
-    // Extract each root and save as (x, y) coordinates
+  
 
     Object.keys(input).forEach(key => {
         if (!isNaN(key)) {
@@ -51,15 +49,15 @@ function solvedequation(filename) {
         }
     });
 
-    // Only use the first k numbers to solve the polynomial
+   
     const requirednumbers = numbers.slice(0, k);
 
-    // Calculate the fixed phase using Lagrange interpolation
+    
     const constantTerm = lagrangeInterpolation(requirednumbers);
 
     console.log(`The secret fixed phase (c) is: ${constantTerm}`);
 }
 
-// Run the solver with the JSON test case
+
 solvedequation('testcase.json');
 solvedequation('testcase2.json');
